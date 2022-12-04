f8f23570783a145b10306793b3a089cd71ddece7
ArinaKozh <arina.mayer66@mail.ru>
2022-12-04 23:02:45 +0300

    задача 23

---
+++ задача_23/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int GetNumber(string message)
+{
+   int result = 0;
+   bool isCorrect = false;
+
+   while(!isCorrect)
+   {
+
+    Console.WriteLine(message);
+
+    if (int.TryParse(Console.ReadLine(), out result) && result>0)
+    {
+        isCorrect = true;
+    } 
+    else
+    {
+        Console.WriteLine("Неверно ввели число");
+    }
+   } 
+   return result;
+}
+int N = GetNumber("Введите натуральное число");
+int i = 1;
+while(i<=N)
+{
+    if (i<N)
+    {
+    Console.Write($"{Math.Pow(i, 3)},  ");
+    i++;
+    }
+    else
+    {
+    Console.Write($"{Math.Pow(i, 3)} ");
+    i++;  
+    }
+}
